eca0a8fde7463eb0d7bd27222e8f86830fab0736
Patrick Golding <patrick.golding@cve.com>
2020-10-30 11:58:21 -0600

    Add API Gateway handler

--- src/Arithmetic/Function.cs
+++ src/Arithmetic/Function.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text.Json;
 
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
 
 using CVE.BasicLambda.Arithmetic;
 using CVE.BasicLambda.Responses;
@@ -22,12 +23,63 @@ namespace CVE.BasicLambda
         /// <returns></returns>
         public string FunctionHandler(Expression input, ILambdaContext context)
         {
+            return ArithmeticExpressionHandler(input);
+        }
+
+        /// <summary>
+        /// A function that handles a basic API Gateway REST event.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public APIGatewayProxyResponse APIGatewayRestHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var response = new APIGatewayProxyResponse();
+
+            response.Headers.Add("content-type", "application/json");
+
+            if (request.HttpMethod == "POST")
+            {
+                response.StatusCode = 200;
+                response.Body = ArithmeticExpressionHandler(JsonSerializer.Deserialize<Expression>(request.Body));
+            }
+            else if (request.HttpMethod == "HEAD"
+                || request.HttpMethod == "GET"
+                || request.HttpMethod == "PUT"
+                || request.HttpMethod == "PATCH"
+                || request.HttpMethod == "DELETE"
+            )
+            {
+                response.StatusCode = 405;
+                response.Headers.Add("Allow", "POST");
+                response.Body = $@"{{
+                    ""error"": ""The HTTP method is not allowed on this resource."",
+                    ""httpMethod"": ""{request.HttpMethod}"",
+                    ""allowedHttpMethods"": [""POST""],
+                    ""resourceRequested"": ""{request.Resource}""
+                }}";
+            }
+            else
+            {
+                response.StatusCode = 400;
+                response.Headers.Add("Allow", "POST");
+                response.Body = $@"{{
+                    ""error"": ""The server does not know the HTTP method that was used."",
+                    ""allowedHttpMethods"": [""POST""]
+                }}";
+            }
+
+            return response;
+        }
+
+        private string ArithmeticExpressionHandler(Expression expression)
+        {
             string resultJsonPayload;
             JsonSerializerOptions serializerOptions = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true };
 
             try
             {
-                var success = new Success() { Result = input.Evaluate() };
+                var success = new Success() { Result = expression.Evaluate() };
                 resultJsonPayload = JsonSerializer.Serialize<Success>(success, serializerOptions);
             }
             catch (Exception exception)
